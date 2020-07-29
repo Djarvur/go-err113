c4007934f5b08a0d3c0d773bde8e9ecfc9d1df2d
Tam Mach <sayboras@yahoo.com>
2020-07-29 23:27:24 +1000

    Fix suggested edit
    Signed-off-by: Tam Mach <sayboras@yahoo.com>

--- testdata/custom.go
+++ testdata/custom.go
@@ -20,24 +20,24 @@ func CustomNotEqual(e1, e2 CustomError) bool {
 
 func CustomCastedEqual(ce1, ce2 CustomError) bool {
 	var e1, e2 error = ce1, ce2
-	return e1 == e2 // want `do not compare errors directly, use errors\.Is\(\) instead: "e1 == e2"`
+	return e1 == e2 // want `do not compare errors directly \"e1 == e2\", use \"errors\.Is\(e1, e2\)\" instead`
 }
 
 func CustomCastedNotEqual(ce1, ce2 CustomError) bool {
 	var e1, e2 error = ce1, ce2
-	return e1 != e2 // want `do not compare errors directly, use errors\.Is\(\) instead: "e1 \!= e2"`
+	return e1 != e2 // want `do not compare errors directly \"e1 != e2\", use \"!errors\.Is\(e1, e2\)\" instead`
 }
 
 func CustomPCastedEqual(ce1, ce2 *CustomError) bool {
 	var e1, e2 error = ce1, ce2
-	return e1 == e2 // want `do not compare errors directly, use errors\.Is\(\) instead: "e1 == e2"`
+	return e1 == e2 // want `do not compare errors directly \"e1 == e2\", use \"errors\.Is\(e1, e2\)\" instead`
 }
 
 func CustomPCastedNotEqual(ce1, ce2 *CustomError) bool {
 	var e1, e2 error = ce1, ce2
-	return e1 != e2 // want `do not compare errors directly, use errors\.Is\(\) instead: "e1 \!= e2"`
+	return e1 != e2 // want `do not compare errors directly \"e1 != e2\", use \"!errors\.Is\(e1, e2\)\" instead`
 }
 
 func CustomHalfCastedEqual(e1 CustomError, e2 error) bool {
-	return e1 == e2 // want `do not compare errors directly, use errors\.Is\(\) instead: "e1 == e2"`
+	return e1 == e2 // want `do not compare errors directly \"e1 == e2\", use \"errors\.Is\(e1, e2\)\" instead`
 }
